fbf6e6e13de79c3ae8d616143369dc714e4493a1
Severin Schols <severin@schols.de>
2016-04-07 12:49:23 +0200

    unicode stuff -.-

--- scripts/tweet.py
+++ scripts/tweet.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# encoding=utf8
 
 import json
 import os
 from datetime import datetime, timedelta, date
 from pytz import timezone
 import tweepy
+import logging
 
 import config
 
 def tweet(status):
-	print "sending tweet (%s): %s" % (len(status), status)
+	logger.info("sending tweet (%s): %s" % (len(status), status))
+
 	try:
 		api.update_status(status)
 	except:
-		print "error sending tweet"
+		logger.error("error sending tweet (%s): %s" % (len(status), status))
+
+logging.basicConfig(filename='tweet.log', format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 directory = os.path.dirname(os.path.realpath(__file__))
 all_events = []
@@ -40,26 +46,26 @@ for source in config.SOURCES:
 	with open("%s/data/%s.json" % (directory, source['name'])) as data_file:
 		events = json.load(data_file)
 		all_events += events
-		print "loading %s ..." % source['title']
+		logger.debug("loading %s" % source['title'])
 
 	for event in events:
 		dt_start = datetime.strptime(event['start'], dt_format_json).replace(tzinfo = tz)
 
 		if dt_start >= time_min and dt_start < time_max:
-			start_out = dt_start.strftime(dt_format_twitter)
+			start_out = dt_start.strftime(dt_format_twitter).decode('utf-8')
 
 			max_length = 140 - 23 - 6 - 16 - len(source['title'])
 			if len(event['title']) > max_length:
-				title = "%s..." % event['title'][:max_length-3]
+				title = u"%s..." % event['title'][:max_length-3]
 			else:
-				title = event['title']
+				title = event['title'].decode('utf-8')
 
 			if "url" in event:
-				text = "%s: %s @ %s %s" % (start_out, title, source['title'], event['url'])
+				text = u"%s: %s @ %s %s" % (start_out, title, source['title'].decode('utf-8'), event['url'])
 			elif "website" in source:
-				text = "%s: %s @ %s %s" % (start_out, title, source['title'], source['website'])
+				text = u"%s: %s @ %s %s" % (start_out, title, source['title'].decode('utf-8'), source['website'])
 			else:
-				text = "%s: %s @ %s" % (start_out, title, source['title'])
+				text = u"%s: %s @ %s" % (start_out, title, source['title'].decode('utf-8'))
 			tweet(text)
 
-print "loaded %s events" % len(all_events)
+logger.debug("loaded %s events" % len(all_events))
